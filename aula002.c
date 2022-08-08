38571605100ce3693fc00cf9faca79dd6086612f
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-08-08 01:47:24 -0300

    aula002 - printf (funcionamento e retorno)

---
+++ aula002.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//Caractere de escape de linha -> \n
+int main(){
+
+    //printf é uma função de saída
+    printf("\nOlá, \nseja \nem-vindo!\n");
+
+    //printf retorna o comprimento da string printada
+    printf("\nValor retornado: %d", printf("Oi\n"));
+}
