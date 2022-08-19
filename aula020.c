8cef70dcc7ea70ef25f347f3ecb843eb8c8af810
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-08-19 00:55:30 -0300

    float na memória

---
+++ aula020.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Aula 020
+//Tamanho de um float na memória
+
+int main(){
+
+	long float x = 0;
+
+	printf("Float na memória = %ld bytes\n", sizeof x);
+	return 0;
+}
