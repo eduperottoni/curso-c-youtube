93d18d2a7a7323091fe1e446ff35266da3593054
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-08-19 21:49:52 -0300

    aula024 - operadores matemáticos

---
+++ aula024.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Aula024
+//Operações matemáticas
+
+int main(){
+	int x = 2;
+	int y = 5;
+	float z = 5;
+	//soma --> +
+	printf("Soma: %d\n", x + y);
+	//multiplicacao --> -
+	printf("Multiplicação: %d\n", x * y);
+	//divisão --> /
+	printf("Divisão: %f\n", z/x);
+	//Divisão de inteiro por inteiro gera um inteiro
+	//subtração --> -
+	printf("Subtração: %d\n", x - y);
+	return 0;
+}
