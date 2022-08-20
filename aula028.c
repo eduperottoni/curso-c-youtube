03e2dc2d343d54d455cc4f3f911d702af2c3b610
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-08-19 22:45:05 -0300

    aula 028 - operador de decremento

---
+++ aula028.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Aula028
+//Operador de decremento --> --;
+
+int main(){
+	int resultado=10, contador=10;
+
+	printf("Valor: %d\n", resultado--);
+	return 0;
+}
