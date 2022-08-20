6d781e3c03ca897d62eb3fe8e9b67de8ea63b72f
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-08-19 22:38:55 -0300

    aula027 - incremento

---
+++ aula027.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Aula 027
+//Operador de incremento -> ++;
+
+int main(){
+	int contador=10, resultado = 10;
+
+	//Pré-incremento --> ++var
+	//Pós-incremento --> var++ (gera uma nova instrução - nova linha)
+
+	//Sinônimos:
+	/*
+		var++;
+		var += 1;
+		var = var + 1;
+	*/
+	resultado = contador++;
+
+	printf("Valor: %d\n", resultado);
+
+	return 0;
+}
